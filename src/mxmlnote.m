8fbfb2bcfcbb8cd25a34cd864675c5c53a4c4947
Patrik Ohlsson <kromatisktkluster@gmail.com>
2013-11-01 12:58:24 +0100

    Started implementation

--- src/mxmlnote.m
+++ src/mxmlnote.m
@@ -2,17 +2,49 @@ classdef mxmlnote
     %UNTITLED7 Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties
+    properties(SetAccess=public)
         pitch;
+        velocity;
         duration;
-        voice;
         notations;
+        voice;
         lyric;
+    end
+    
+    properties(SetAccess=private, Hidden)
+        starts;
+        ends;
         positioninbar;
     end
     
     methods
+        function obj = mxmlnote(pitch, velocity, duration, varargin)
+            obj.pitch = pitch;
+            obj.velocity = velocity;
+            obj.duration = duration;
+            switch(length(varargin))
+                case 1
+                    obj.notations = varargin{1};
+                case 2
+                    obj.notations = varargin{1};
+                    obj.voice = varargin{2};
+                case 3
+                    obj.notations = varargin{1};
+                    obj.voice = varargin{2};
+                    obj.lyric = varargin{3};
+                otherwise
+                    if(length(varargin)>3)
+                        error('Argument error: %s', 'Wrong argument count');
+                    end
+            end
+        end
     end
     
+    methods(Access=private)
+        function obj = boundfix(obj)
+            % make sure length(pitch) == length(velocity) ==
+            % length(duration)
+        end 
+    end
 end
 
